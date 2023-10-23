140ba355c228f22c8afda7aa615b5ed50ee744c9
Christian Martinez <christianandremartinez@gmail.com>
2023-10-23 16:13:10 -0600

    hoysi

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,24 @@
-import "./Task.css"
-export default function Task(props) {
+  import { useEffect, useState } from "react";
+  import "./Task.css"
+  export default function Task(props) {
 
+  const [complete, setComplete] = useState(false);
 
-    return (
-      <div className="tasks">
-        <h2>{props.task.name}</h2>
-        <p>{props.task.description}</p>
-        <p>Completada: {props.task.completada ? "Si" : "No"}</p>
-      </div>
-    );
+  const changeTask = () => {
+    setComplete(!complete)
   }
+
+  useEffect(() => {
+    console.log("cambio en la asignación de la tarea");
+  }, [complete]);
+
+
+      return (
+        <div className="tasks">
+          <h2>{props.task.name}</h2>
+          <p>{props.task.description}</p>
+          <p>Completada: {complete ? "Si" : "No"}</p>
+          <button onClick={changeTask}>Asignar</button>
+        </div>
+      );
+    }
